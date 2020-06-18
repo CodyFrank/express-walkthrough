86e3c178ccb3a3a93c3c2a24824b21a04a5c3271
Cody Frank <cody.frank30@gmail.com>
2020-06-18 13:56:02 -0400

    converts to static folder

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.get( '/', (req, res)=>{
-    res.sendFile(path.join( __dirname, 'public', 'index.html'))
-})
+app.use(express.static(__dirname, 'public'))
+
+// app.get( '/', (req, res)=>{
+//     res.sendFile(path.join( __dirname, 'public', 'index.html'))
+// })
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`))
