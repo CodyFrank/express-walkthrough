51820e5d5cab5df3d3dec3d0b4745c094575eda8
Cody Frank <cody.frank30@gmail.com>
2020-06-18 14:28:00 -0400

    create middleware logger

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const app = express()
 const path = require('path')
 const members = require('./Members')
 
+const logger = (req, res, next) => {
+    console.log(`${req.prot}:// ${req.get('host')}${req.originalUrl}`)
+    next()
+}
+
+app.use(logger)
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/api/members', (req, res)=>{
     res.json(members)
 })
 
-// app.get( '/', (req, res)=>{
-//     res.sendFile(path.join( __dirname, 'public', 'index.html'))
-// })
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`))
