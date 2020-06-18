d7293949d15d8a637bb65c68f4a760c0090a67e3
Cody Frank <cody.frank30@gmail.com>
2020-06-18 14:08:00 -0400

    hard codes in some members data

--- index.js
+++ index.js
@@ -2,7 +2,29 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const members = [
+    {
+        id: 1,
+        name: "Spiderman",
+        age: 17
+    },
+    {
+        id: 2,
+        name: "Ironman",
+        age: 44
+    },
+    {
+        id: 3,
+        name: "Nick Fury",
+        age: 58
+    }
+]
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/api/members', (req, res)=>{
+
+})
 
 // app.get( '/', (req, res)=>{
 //     res.sendFile(path.join( __dirname, 'public', 'index.html'))
