38176200cb8c4368ae932ae3609b34b1e8074d57
Cody Frank <cody.frank30@gmail.com>
2020-06-18 13:52:11 -0400

    renders html from a view file for /root route

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.get( '/', (req, res)=>{
-    res.send("<h1>Hello World!!</h1>")
+    res.sendFile(path.join( __dirname, 'public', 'index.html'))
 })
 
 const PORT = process.env.PORT || 3000
