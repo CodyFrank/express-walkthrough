5413837351bbbdb97eb084b6745263846b0a372e
Cody Frank <cody.frank30@gmail.com>
2020-06-18 15:02:05 -0400

    creates member show route

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.get('/api/members', (req, res)=>{
     res.json(members)
 })
 
+app.get('/api/members/:id', (req, res)=>{
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    } else {
+        res.status(400).json({ msg: `No member found with id of ${req.params.id}`})
+    }
+})
+
 
 
 const PORT = process.env.PORT || 3000
